23918b1ec452a6114d5fd4ef82043496d393b178
Vladislav <vladislavgulak00@gmail.com>
2023-10-20 19:58:08 +0300

    Add new models

--- djangosite/metrics/models.py
+++ djangosite/metrics/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    logtitude = models.FloatField()
+    altitude = models.FloatField()
+
+class Unit_Mesure(models.Model):
+    name = models.CharField(max_length=255)
+
+class Measurement(models.Model):
+    name = models.CharField(max_length=255)
+    description= models.CharField(max_length=255)
+    unit = models.ForeignKey(Unit_Mesure,on_delete=models.CASCADE)
+
+class Timestamps(models.Model):
+    measurement = models.ForeignKey(Measurement,on_delete=models.CASCADE)
+    value = models.FloatField()
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    time = models.DateTimeField()
+
