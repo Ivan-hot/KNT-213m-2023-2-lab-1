8542daeef90a221beea5929ef029b7a968243728
Dmytro Kolbasa <dmytro.kolbasa@celadonsoft.com>
2023-10-21 22:58:52 +0300

    #settings #urls

--- djangosite/djangosite/urls.py
+++ djangosite/djangosite/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path
 from metrics import views
+
+
 urlpatterns = [
      path("admin/", admin.site.urls),
     path("", views.index)
-    
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
